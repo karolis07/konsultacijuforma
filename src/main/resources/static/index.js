8246db4de41295a92846dc43ac7b0272115f06b4
markussohn <markus.justinas@gmail.com>
2016-02-15 15:52:12 +0200

    Routes work

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,3 +1,29 @@
 /**
  * Created by p998snc on 2016.02.10.
  */
+var demoApp = angular.module('demoApp',['ngRoute']);
+
+    demoApp.config(['$routeProvider', function ($routeProvider){
+        $routeProvider
+            .when('/',
+            {
+                controller: 'SimpleController',
+                templateUrl: 'home.html'
+            })
+            .when('/newReg',
+            {
+                controller: 'SimpleController',
+                templateUrl: 'newRegistration.html'
+            })
+            .when('/contact',
+            {
+                controller: 'SimpleController',
+                templateUrl: 'contact.html'
+            })
+            .when('/history',
+                        {
+                            controller: 'SimpleController',
+                            templateUrl: 'history.html'
+                        })
+            .otherwise({ redirectTo: '/'});
+        }]);
