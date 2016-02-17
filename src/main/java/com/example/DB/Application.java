3500ce303beec63dac7e5dbcf24b5e52c5f5ef9f
karolis07 <karolis0731@gmail.com>
2016-02-17 16:32:37 +0200

    test Database

---
+++ src/main/java/com/example/DB/Application.java
@@ -0,0 +1,15 @@
+package com.example.DB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by karolis on 2/17/16.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
