bc56e3133f16d2a3ffee6c652d0670593dee41da
karolis0731 <karolis0731@gmail.com>
2016-02-18 11:02:57 +0200

    Working DB

--- src/main/java/com/example/Service/NewService.java
+++ src/main/java/com/example/Service/NewService.java
@@ -30,6 +30,17 @@ class newService {
         ctx.register(MongoDBConfig.class);
         ctx.refresh();
         employeeRepository = ctx.getBean(EmployeeRepository.class);
+        Employee ram = new Employee(1,"Ram",20);
+        Employee shyam = new Employee(2,"Shyam",19);
+        Employee mohan = new Employee(3,"Mohan",20);
+        Employee krishn = new Employee(4,"Krishn",20);
+        //Delete if exists already
+        employeeRepository.deleteAll();
+        //Save employee
+        employeeRepository.save(ram);
+        employeeRepository.save(shyam);
+        employeeRepository.save(mohan);
+        employeeRepository.save(krishn);
     }
 
     @RequestMapping("/peanuts")
