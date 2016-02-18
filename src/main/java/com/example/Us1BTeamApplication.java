4ac7b6a0bb07f17fc23173d75fd48510fb494dfb
karolis0731 <karolis0731@gmail.com>
2016-02-18 10:32:07 +0200

    Working DB

--- src/main/java/com/example/Us1BTeamApplication.java
+++ src/main/java/com/example/Us1BTeamApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Us1BTeamApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Us1BTeamApplication.class, args);
-	}
+    public static void runSpring(String[] args)
+    {
+        SpringApplication.run(Us1BTeamApplication.class,args);
+    }
 }
