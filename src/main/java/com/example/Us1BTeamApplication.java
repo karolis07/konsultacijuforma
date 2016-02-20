30e2153b27db7419ea7084e3e35384d8cece4ed8
karolis0731 <karolis0731@gmail.com>
2016-02-20 19:43:59 +0200

    initial

--- src/main/java/com/example/Us1BTeamApplication.java
+++ src/main/java/com/example/Us1BTeamApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Us1BTeamApplication {
 
-    public static void runSpring(String[] args)
+    public static void main(String[] args)
     {
         SpringApplication.run(Us1BTeamApplication.class,args);
     }
