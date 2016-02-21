f5afc6bc0a1e9c61c203002c548194ccc4dc6f8a
Karolas <karolisb694@gmail.com>
2016-02-21 22:54:24 +0200

    try register

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -112,7 +112,7 @@ var demoApp = angular.module('demoApp',['ngRoute']);
 
            $scope.submit = function(){
                 $http.put('/US2/' + $scope.name + '/' + $scope.surname + '/' + $scope.tel + '/' + $scope.email + '/' + $scope.bank + '/' + $('#date').val() + '/' + $scope.subject + '/' + $scope.message);
-                $location.url('/home');
+                //$location.url('/home');
             //$location.url('/home');
                 //window.location.href = 'google.com';
            };
