940b27b9880036bc7490a006cd58672ea1ce5077
Karolas <karolisb694@gmail.com>
2016-02-21 16:49:09 +0200

    try register

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -96,7 +96,7 @@ var demoApp = angular.module('demoApp',['ngRoute']);
         demoApp.controller('testControl', function($scope, $location){
             $scope.submit = function(){
                 window.location.href = 'http://swedbank-us1bteam.rhcloud.com/#/history';
-                window.location.href = 'http://swedbank-us1bteam.rhcloud.com/insert?id=' + $scope.id + '&name=' + $scope.name + '&surname=' + $scope.surname;
+                setTimeout(window.location.href = 'http://swedbank-us1bteam.rhcloud.com/insert?id=' + $scope.id + '&name=' + $scope.name + '&surname=' + $scope.surname, 3000)
                 //$location.url('/home');
             };
 
