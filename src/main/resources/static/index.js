3db04bf91532a9985f0d58ddb705f88a6cd568ea
Karolas <karolisb694@gmail.com>
2016-02-22 00:07:44 +0200

    try register

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,7 +1,7 @@
 /**
  * Created by p998snc on 2016.02.10.
  */
-var demoApp = angular.module('demoApp',['ngRoute']);
+var demoApp = angular.module('demoApp',['ngRoute', 'ngMaterial', 'ngMessages']);
 
     demoApp.config(function($routeProvider){
         $routeProvider
@@ -124,6 +124,20 @@ var demoApp = angular.module('demoApp',['ngRoute']);
             //$location.url('/home');
                 //window.location.href = 'google.com';
            };
+
+           $scope.myDate = new Date();
+             $scope.minDate = new Date(
+                 $scope.myDate.getFullYear(),
+                 $scope.myDate.getMonth() - 2,
+                 $scope.myDate.getDate());
+             $scope.maxDate = new Date(
+                 $scope.myDate.getFullYear(),
+                 $scope.myDate.getMonth() + 2,
+                 $scope.myDate.getDate());
+             $scope.onlyWeekendsPredicate = function(date) {
+               var day = date.getDay();
+               return day === 0 || day === 6;
+             }
 
 //           angular.element(document).ready(function () {
 //                var date_input=$('input[name="date"]'); //our date input has the name "date"
