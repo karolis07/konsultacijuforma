4377e1c4ca1880b29be96883c89745a4019eef7a
karolis0731 <karolis0731@gmail.com>
2016-02-22 13:03:48 +0200

    Languages LT +EN list

--- src/main/java/com/example/Config/Language.java
+++ src/main/java/com/example/Config/Language.java
@@ -4,10 +4,12 @@ import java.util.*;
 
 public class Language {
    //private List<String> fieldsNames = new ArrayList<String>();
-    private Map<String,String> fieldsNames = new HashMap<String, String>();
+    public Map<String,String> fieldsNames;
 
-    public void getLT()
+    public Map<String,String> getLT()
     {
+        fieldsNames = new HashMap<String, String>();
+
         // LOGIN
         fieldsNames.put("titleOne","IT Akademijos Bankas");
         fieldsNames.put("titleTwo","Fianansų konsultacijos");
@@ -94,5 +96,101 @@ public class Language {
         // MISC
         fieldsNames.put("buttonSend","Siųsti");
         fieldsNames.put("buttonBack","< Atgal");
+
+        return fieldsNames;
+    }
+
+    public Map<String,String> getEN()
+    {
+        fieldsNames = new HashMap<String, String>();
+
+        // LOGIN
+        fieldsNames.put("titleOne","IT Akademy Bank");
+        fieldsNames.put("titleTwo","Financial consults");
+
+        fieldsNames.put("loginTitle","LOG IN");
+        fieldsNames.put("loginEmail","E. Mail");
+        fieldsNames.put("loginPassword","Password");
+        fieldsNames.put("loginButtonLogin","Submit");
+        fieldsNames.put("loginButtonRegister","Register");
+        fieldsNames.put("loginCookie","Keep me loged in");
+
+        fieldsNames.put("loginError","There is no such a e. mail");
+        fieldsNames.put("loginError2","Wrong password");
+
+        //  HOME
+
+        fieldsNames.put("homeTitle", "CONTACT US");
+        fieldsNames.put("homeButtonWrite", "Write to us");
+        fieldsNames.put("homeButtonRegister", "Register");
+        fieldsNames.put("homeButtonHistory", "My Registrations");
+        fieldsNames.put("homeFooterText","If your question relates to the financial information of the IT Academy Bank by phoning 1234 4321 information is available from your set as a bank client's identity.");
+
+        //  CONTACT
+
+        fieldsNames.put("contactTitle","Write to us");
+        fieldsNames.put("contactMessage","Message text");
+        fieldsNames.put("contactAnswer","How would you like to get feedback");
+        fieldsNames.put("contactAnswerList1","Phone");
+        fieldsNames.put("contactAnswerList2","E. mail");
+        fieldsNames.put("contactAnswerList3","E. mail and Phone");
+        fieldsNames.put("contactFooterText","Answers to questions raised on weekdays 8-20 pm., Saturdays 9-16 hours. If you need immediate" +
+                "Information , waiting for your call tel . 1884 ( for residential customers ) and 1633 ( for business customers ) . From abroad call tel . " +
+                "+370 5 268 4444 ( For residential customers ) and +370 5 268 4422 (business customers). ");
+
+        fieldsNames.put("contactError","Fill in the required fields");
+
+        // CONTACT CONFIRMATION
+
+        fieldsNames.put("contactConfirmText","Welcome,\n" +
+                "Thank you for contacting the IT Academy Bank . \n" +
+                "Answers to questions raised on weekdays 8-20 pm . , Saturdays 9-16 hours. If you need immediate information \n" +
+                "We are waiting for your phone call 1884 or tel . +370 5 268 4444 if you are calling from abroad. We wish a good day !");
+
+        //  REGISTRATION
+
+        fieldsNames.put("registrationTitle","Register for consult on the internet");
+        fieldsNames.put("registrationBankDepartment","Bank Department");
+        fieldsNames.put("registrationBankDepartmentList1","Antakalnio g. 45");
+        fieldsNames.put("registrationBankDepartmentList2","Ateities g. 91 (PC Mandarinas)");
+        fieldsNames.put("registrationBankDepartmentList3","Gedimino pr. 56");
+        fieldsNames.put("registrationBankDepartmentList4","Konstitucijos pr. 20A, works Būsto Centras");
+        fieldsNames.put("registrationBankDepartmentList5","Ozo g. 25 (PPC „Akropolis“)");
+        fieldsNames.put("registrationDate","Date");
+        fieldsNames.put("registrationTime","Consult time");
+        fieldsNames.put("registrationMessage","Note, comments");
+
+        //  REGISTRATION CONFIRMATION
+
+        fieldsNames.put("registrationConfirmationTitle","Registration confirmation");
+        fieldsNames.put("registrationFooterText", "Thank you for your interest in the bank.\n" +
+                "If you are in need of urgent information , waiting for your call by phone 1884 , weekdays 8-20 pm . , Saturdays 9-16 hours .\n");
+
+        // REGISTRATION AND CONTACT
+
+        fieldsNames.put("name","Name");
+        fieldsNames.put("surname","Surname");
+        fieldsNames.put("phone","Phone");
+        fieldsNames.put("email","E. mail");
+
+        fieldsNames.put("theme","Theme");
+        fieldsNames.put("themeList1","Savings and investments;");
+        fieldsNames.put("themeList2","Pensions and accumulation;");
+        fieldsNames.put("themeList3","Life insurance;");
+        fieldsNames.put("themeList4","Non-life insurance;");
+        fieldsNames.put("themeList5","loans and leasing;");
+        fieldsNames.put("themeList6","The daily financial management.");
+
+        // HISTORY
+
+        fieldsNames.put("historyTitle","Registration through internet");
+        fieldsNames.put("historyButtonLook","Review");
+        fieldsNames.put("historyButtonDelete","Cancel");
+
+        // MISC
+        fieldsNames.put("buttonSend","Send");
+        fieldsNames.put("buttonBack","< Back");
+
+        return fieldsNames;
     }
 }
